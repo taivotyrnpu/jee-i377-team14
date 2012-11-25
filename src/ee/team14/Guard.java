370b4ad0a81f435490e0dc8ba63300a7215fe59d
Jarko <Jarko@Jarko-PC.lan>
2012-11-25 18:18:22 +0200

    klass valvur

---
+++ src/ee/team14/Guard.java
@@ -0,0 +1,15 @@
+package ee.team14;
+
+public class Guard {
+	String nimi;
+	int vanus;
+	String aadress;
+	int hulk
+	
+    Guard(String n, int v, String a, int h){
+        this.nimi = n;
+        this.vanus = v;
+        this.aadress = a;
+        this.hulk = h;
+    }
+}
